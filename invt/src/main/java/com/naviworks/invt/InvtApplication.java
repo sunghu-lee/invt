0a5f230fec5b282b49f6c1419f887f02caf86b25
naviworks <naviworks@invt-server.kip20heint1udmdhhsdanvz3mb.syx.internal.cloudapp.net>
2020-01-12 13:57:42 +0000

    초기 프로젝트 커밋 1. 정보검색 서비스 완료(이성후)

---
+++ invt/src/main/java/com/naviworks/invt/InvtApplication.java
@@ -0,0 +1,16 @@
+package com.naviworks.invt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+public class InvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvtApplication.class, args);
+	}
+
+}
