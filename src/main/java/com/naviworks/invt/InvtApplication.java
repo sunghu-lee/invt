7810ace5c72b5b38d241fa3ba8aa6c0e26682b94
naviworks <naviworks@invt-server.kip20heint1udmdhhsdanvz3mb.syx.internal.cloudapp.net>
2020-01-14 15:14:56 +0000

    filter

--- src/main/java/com/naviworks/invt/InvtApplication.java
+++ src/main/java/com/naviworks/invt/InvtApplication.java
@@ -4,10 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.naviworks.invt.filter.CORSFilter;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class InvtApplication {
+	
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer
+	{
+	    @Bean
+	    public FilterRegistrationBean getFilterRegistrationBean()
+	    {
+	        FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+	        registrationBean.addUrlPatterns("/Search");
+	        registrationBean.addUrlPatterns("/Search/*");
+	        return registrationBean;
+	    }
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(InvtApplication.class, args);
